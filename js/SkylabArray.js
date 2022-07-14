a22e546e808444c714470bd2105f84c0983fca62
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-14 22:16:35 +0200

    Added push and lenght method for our class array

--- js/SkylabArray.js
+++ js/SkylabArray.js
@@ -1,20 +1,17 @@
-let i = 0;
 class SkylabArray {
   constructor(...values) {
+    let i = 0;
     do {
       this[i] = values[i];
       i += 1;
     } while (values[i] !== undefined);
+    this.length = i;
   }
 
-  // length() {
-  //   i = 0;
-  //   do {
-  //     this[i] = this.values[i];
-  //     i += 1;
-  //   } while (this.values[i] !== undefined);
-  //   console.log(i);
-  // }
+  push(pushedObj) {
+    this[this.length] = pushedObj;
+    this.length += 1;
+  }
 }
 
 module.exports = SkylabArray;
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const SkylabArray = require("./SkylabArray");
 
-const newArray = new SkylabArray(10, "lol");
-
-// newArray.length();
+const newArray = new SkylabArray(10, "lol", 666);
+console.log(newArray);
+console.log(newArray.length);
+newArray.push("cigui");
+console.log(newArray);
