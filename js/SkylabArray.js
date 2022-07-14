897afd60c145a5d54c350a4998c1bed56beac2b7
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-14 21:23:02 +0200

    Switched back to common JS module

--- js/SkylabArray.js
+++ js/SkylabArray.js
@@ -6,6 +6,15 @@ class SkylabArray {
       i += 1;
     } while (values[i] !== undefined);
   }
+
+  // length() {
+  //   i = 0;
+  //   do {
+  //     this[i] = this.values[i];
+  //     i += 1;
+  //   } while (this.values[i] !== undefined);
+  //   console.log(i);
+  // }
 }
 
-export default SkylabArray;
+module.exports = SkylabArray;
