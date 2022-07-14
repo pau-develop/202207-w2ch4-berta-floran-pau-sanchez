cebb38fcbfcff766b325da0c600082beaee19173
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-14 23:17:36 +0200

    Added filther method to SkylabArray class

--- js/index.js
+++ js/index.js
@@ -4,4 +4,7 @@ const newArray = new SkylabArray(10, "lol", 666);
 console.log(newArray);
 console.log(newArray.length);
 newArray.push("cigui");
+newArray.push("cigui");
 console.log(newArray);
+const filterArray = newArray.filter("cigui");
+console.log(filterArray);
